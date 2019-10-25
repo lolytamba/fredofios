a950e9d10c5037355fb8c2c215a5268a0fed5082
Your Name <tlolyelia@gmail.com>
2019-10-25 20:58:52 +0700

    home

---
+++ resources/js/routes.js
@@ -0,0 +1,10 @@
+import App from './components/App'
+import Home from './components/Home'
+
+export const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+  ];
